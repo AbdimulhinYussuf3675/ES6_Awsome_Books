94510d34b9fd268fc600f85403408bede0b8819a
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-01-17 08:36:06 +0300

    Add js for module

---
+++ index.js
@@ -0,0 +1,11 @@
+/* add nav for SPA */
+import bookLink from './modules/nav.js';
+
+/* add a book from the list book */
+import appendBook from './modules/addBooks.js';
+
+/* remove a book from the list */
+import addRemoveListener from './modules/removeBooks.js';
+
+/* add time and date */
+import dateAndTime from './modules/time.js';
